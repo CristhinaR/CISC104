c80578f9391c9b176be7afc0aa47d420c70004a1
CristhinaR <113139499+CristhinaR@users.noreply.github.com>
2022-10-27 16:28:14 -0400

    My Project 1

---
+++ My project (2)/Assets/Scripts/rockpaperscissors.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RockPaperScissors
+{
+
+    public TMP_Text result;
+    public Image AIChoice;
+
+
+    public string[] Choices;
+    public Sprite Rock, Paper, Scissors;
+
+
+
+    public void Play(string myChoice)
+    {
+        string randomChoice = Choices[Random.Range(0, Choices.Length)];
+
+        switch (randomChoice)
+        {
+
+            case "Rock":
+                switch (myChoice)
+                {
+                    case "Rock":
+                        result.text = "Tie";
+                        break;
+
+                    case "Paper":
+                        result.text = "You Win!!";
+
+                        break;
+                    case "Scissors":
+                        result.text = "You Lose :(";
+                        break;
+
+
+                }
+
+                AIChoice.sprite = Rock;
+                break;
+
+            case "Paper":
+                switch (myChoice)
+                {
+
+                    case "Rock":
+                        result.text = "You Win!!";
+                        break;
+
+                    case "Paper":
+                        result.text = "Tie";
+                        break;
+
+                    case "Scissors":
+                        result.text = "You Lose:(";
+                        break;
+
+                }
+
+                AIChoice.sprite = Paper;
+                break;
+
+            case "Scissors":
+                switch (myChoice)
+                {
+                    case "Rock":
+                        result.text = "You Lose:(";
+                        break;
+
+                    case "Paper":
+                        result.text = "You Win!!";
+                        break;
+
+                    case "Scissors":
+                        result.text = "Tie";
+                        break;
+
+                }
+
+                AIChoice.sprite = Scissors;
+                break;
+
+
+
+
+
+        }
+
+    }
+}  
+
---
+++ My project (2)/Assets/Tests/Tests/RockPaperScissorsTest.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+//public class RockPaperScissorsTest
+//{
+    //private object Result;
+
+    // A Test behaves as an ordinary method
+    //[Test]
+   //public void RockPaperScissorsTestSimplePasses()
+   //{
+       // rockpaperscissors game = new rockpaperscissors();
+       //Result result = game.SetChoices(GameChoices.rock, GameChoices.scissors);
+       // Assert.Equals(Result.win, result);
+        
+
+
+   //}
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+   //[UnityTest]
+   // public IEnumerator RockPaperScissorsTestWithEnumeratorPasses()
+    //{
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        //yield return null;
+   // }
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}
+// A Test behaves as an ordinary method
+// [Test]
+// public void RockPaperScissorsTestSimplePasses()
+// {
+
+// Assert.AreEqual 
+//}
+
+// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+// `yield return null;` to skip a frame.
+// [UnityTest]
+// public IEnumerator RockPaperScissorsTestWithEnumeratorPasses()
+//{
+// Use the Assert class to test conditions.
+// Use yield to skip a frame.
+// yield return null;
+//}
+//}
